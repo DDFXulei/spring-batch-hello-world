6eca7f924c75397f82a6f91f850c8ffc33cc35ac
XuLei <xul4@wicresoft.com>
2020-09-29 17:06:10 +0800

    Initial commit

---
+++ src/test/java/com/josephwork/springbatchhelloworld/SpringBatchHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josephwork.springbatchhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
